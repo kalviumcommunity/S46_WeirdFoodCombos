e0898b7e3de9dcf3976abdcb59fb46ed6818111d
vedha <vedh1305@gmail.com>
2024-02-14 11:24:39 +0530

    Update

--- client/WeirdEats/src/App.jsx
+++ client/WeirdEats/src/App.jsx
@@ -1,16 +1,18 @@
 /* eslint-disable react/no-unescaped-entities */
 import './App.css'
-import bg from './Assets/Bg.png'
-import Login from './components/Login'
+// import bg from './Assets/Bg.png'
+// import Login from './components/Login'
+import MainContent from './components/MainContent'
 
 function App() {
 
   return (
     <>
     <div>
-    <div className="bg-cover bg-center" style={{ backgroundImage: `url(${bg})`, minHeight: '100vh' }}>
+    {/* <div className="bg-cover bg-center" style={{ backgroundImage: `url(${bg})`, minHeight: '100vh' }}>
       <Login/>
-    </div>
+    </div> */}
+    <MainContent/>
     </div>
     </>
   )
